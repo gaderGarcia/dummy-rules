8c1b2a08f5f9082b5bccbd310e94a540df826d12
Edgar Garcia <edgar.garcia@vesta.io>
2024-03-14 13:01:30 -0600

    Adding a dummy Controller

--- rules/controller_100321.py
+++ rules/controller_100321.py
@@ -1,3 +1,9 @@
+from pydantic import BaseModel
+
 class Controller:
-    def execute(self):
-        print("Something to print")
+    def __init__(self, BaseModel) -> None:
+        pass
+
+    def execute(self, transaction:BaseModel, meta_data: dict):
+        print(f"Something to print:{transaction.transactionId}, metadata:{meta_data}")
+        transaction.transactionEvaluation.oneStepPostAuth["score"] = 88
