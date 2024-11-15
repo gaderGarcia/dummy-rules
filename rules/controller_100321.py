86dd00dcdab0a033b06ca03dde128ee401208545
Edgar <admin@MacBook-Pro-de-admin.local>
2024-11-15 12:48:38 -0600

    Revert "Adding a dummy controller"
    This reverts commit 1a3832eb58277b0d1007ec6899333bb50033f256.

--- rules/controller_100321.py
+++ rules/controller_100321.py
@@ -1,39 +1,9 @@
 from pydantic import BaseModel
-from abc import ABC,abstractmethod
 
-class IStageIterator(ABC):
-    @abstractmethod
-    def __iter__(self):
-        return self
-
-    @abstractmethod
-    def __next__(self):
-        raise StopIteration
-
-    @abstractmethod
-    def get_remaining_stages(self):
-        return []
-
-class IController(ABC):
-
-    def __init__(self) -> None:
-        super().__init__()
-
-    @abstractmethod
-    def getStageIterator(self, startingStage, transaction) -> IStageIterator:
-        pass
-
-    @abstractmethod
-    def execute(self, transaction, transaction_meta_data):
-        pass
-
-class Controller(IController):
-    def __init__(self, transaction) -> None:
+class Controller:
+    def __init__(self, BaseModel) -> None:
         pass
 
     def execute(self, transaction:BaseModel, meta_data: dict):
         print(f"Something to print:{transaction.transactionId}, metadata:{meta_data}")
         transaction.transactionEvaluation.oneStepPostAuth["score"] = 88
-
-    def getStageIterator(self, startingStage, transaction) -> IStageIterator:
-        pass
